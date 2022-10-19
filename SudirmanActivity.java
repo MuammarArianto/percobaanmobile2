5d7027e9a9cbd3698fe1a363d3766ddcc9c54a13
MuammarArianto <114142566+MuammarArianto@users.noreply.github.com>
2022-10-19 19:09:43 +0800

    Add files via upload

---
+++ SudirmanActivity.java
@@ -0,0 +1,14 @@
+package com.Muammar_F55121066.mylistviewmuammar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SudirmanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sudirman);
+    }
+}
